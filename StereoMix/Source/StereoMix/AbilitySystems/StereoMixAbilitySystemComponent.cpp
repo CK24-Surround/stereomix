7b318adf7f74384afa954ac1ca5367f631ca3506
TD 이동우 <mintyway0112@gmail.com>
2024-04-10 04:55:11 +0900

    ASC 수정

--- StereoMix/Source/StereoMix/AbilitySystems/StereoMixAbilitySystemComponent.cpp
+++ StereoMix/Source/StereoMix/AbilitySystems/StereoMixAbilitySystemComponent.cpp
@@ -4,6 +4,15 @@
 #include "StereoMixAbilitySystemComponent.h"
 
 #include "Characters/StereoMixPlayerCharacter.h"
+#include "Net/UnrealNetwork.h"
+
+void UStereoMixAbilitySystemComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UStereoMixAbilitySystemComponent, CurrentHoldingPawn);
+	DOREPLIFETIME(UStereoMixAbilitySystemComponent, CurrentHolderPawn);
+}
 
 void UStereoMixAbilitySystemComponent::OnTagUpdated(const FGameplayTag& Tag, bool TagExists)
 {
--- StereoMix/Source/StereoMix/AbilitySystems/StereoMixAbilitySystemComponent.h
+++ StereoMix/Source/StereoMix/AbilitySystems/StereoMixAbilitySystemComponent.h
@@ -13,9 +13,28 @@ class STEREOMIX_API UStereoMixAbilitySystemComponent : public UAbilitySystemComp
 {
 	GENERATED_BODY()
 
+public:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
 protected:
 	virtual void OnTagUpdated(const FGameplayTag& Tag, bool TagExists) override;
 
 public:
 	FOnChangedTagSignature OnChangedTag;
+
+public:
+	// 자신이 잡고 있는 폰을 반환합니다.
+	FORCEINLINE APawn* GetCurrentHoldingPawn() { return CurrentHoldingPawn.Get(); }
+
+	// 자신이 잡혀 있는 폰을 반환합니다.
+	FORCEINLINE APawn* GetCurrentHolderPawn() { return CurrentHolderPawn.Get(); }
+
+protected:
+	// 자신이 잡고 있는 폰을 의미합니다.
+	UPROPERTY(Replicated)
+	TWeakObjectPtr<APawn> CurrentHoldingPawn;
+
+	// 자신이 잡혀 있는 폰을 의미합니다.
+	UPROPERTY(Replicated)
+	TWeakObjectPtr<APawn> CurrentHolderPawn;
 };
