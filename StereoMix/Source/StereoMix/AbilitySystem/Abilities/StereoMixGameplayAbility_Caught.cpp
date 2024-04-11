da82ac7e82406e22048e88e4205d5220bca24418
TD 이동우 <mintyway0112@gmail.com>
2024-04-12 00:05:24 +0900

    Caught 개선

--- StereoMix/Source/StereoMix/AbilitySystem/Abilities/StereoMixGameplayAbility_Caught.cpp
+++ StereoMix/Source/StereoMix/AbilitySystem/Abilities/StereoMixGameplayAbility_Caught.cpp
@@ -26,15 +26,12 @@ void UStereoMixGameplayAbility_Caught::ActivateAbility(const FGameplayAbilitySpe
 	
 	// 재생을 시도하고 재생에 실패했다면 bWasCancelled = true로 종료합니다.
 	const float Duration = SourceASC->PlayMontage(this, ActivationInfo, CaughtMontage, 1.0f);
-	if (ensure(Duration > 0.0f))
-	{
-		CommitAbility(Handle, ActorInfo, ActivationInfo);
-	}
-	else
+	if (!ensure(Duration > 0.0f))
 	{
 		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
 		return;
 	}
 	
+	CommitAbility(Handle, ActorInfo, ActivationInfo);
 	EndAbility(Handle, ActorInfo, ActivationInfo, true, false);
 }
