36c020662131e0a64b4c29258eef607a5320ec26
TD 이동우 <mintyway0112@gmail.com>
2024-04-12 16:38:44 +0900

    로그 개선

--- StereoMix/Source/StereoMix/Utilities/StereoMixLog.h
+++ StereoMix/Source/StereoMix/Utilities/StereoMixLog.h
@@ -13,34 +13,38 @@ DECLARE_LOG_CATEGORY_CLASS(LogStereoMixNetwork, Log, All);
 	FString Macro_LocalRoleInfo;\
 	FString Macro_RemoteRoleInfo;\
 	const FString Macro_FunctionInfo = FString(__FUNCTION__);\
-	\
+\
 	if (Macro_NetOwner)\
 	{\
 		const ENetMode Macro_NetMode = Macro_NetOwner->GetNetMode();\
 		Macro_NetModeInfo = Macro_NetMode == NM_Client ? FString::Printf(TEXT("Client%d"), static_cast<int>(GPlayInEditorID)) : Macro_NetMode == NM_DedicatedServer ? TEXT("Server") : TEXT("Misc");\
-		\
-		Macro_LocalRoleInfo = UEnum::GetValueAsString(TEXT("Engine.ENetRole"), Macro_NetOwner->GetLocalRole());\
-		Macro_RemoteRoleInfo = UEnum::GetValueAsString(TEXT("Engine.ENetRole"), Macro_NetOwner->GetRemoteRole());\
+\
+		const ENetRole Macro_LocalRole = Macro_NetOwner->GetLocalRole();\
+		Macro_LocalRoleInfo = Macro_LocalRole == ROLE_Authority ? TEXT("Auth") : Macro_LocalRole == ROLE_AutonomousProxy ? TEXT("Auto") : Macro_LocalRole == ROLE_SimulatedProxy ? TEXT("Sim") : TEXT("None");\
+\
+		const ENetRole Macro_RemoteRole = Macro_NetOwner->GetRemoteRole();\
+		Macro_RemoteRoleInfo = Macro_RemoteRole == ROLE_Authority ? TEXT("Auth") : Macro_RemoteRole == ROLE_AutonomousProxy ? TEXT("Auto") : Macro_RemoteRole == ROLE_SimulatedProxy ? TEXT("Sim") : TEXT("None");\
 	}\
-	\
+\
 	FString Macro_Log;\
 	if (Macro_NetOwner)\
 	{\
-	Macro_Log = FString::Printf(TEXT("[%s] [Local: %s | Remote: %s] [%s]"), *Macro_NetModeInfo, *Macro_LocalRoleInfo, *Macro_RemoteRoleInfo, *Macro_FunctionInfo);\
+		Macro_Log = FString::Printf(TEXT("[%s] [Loc(%s)|Rem(%s)] [%s]"), *Macro_NetModeInfo, *Macro_LocalRoleInfo, *Macro_RemoteRoleInfo, *Macro_FunctionInfo);\
 	}\
 	else\
 	{\
 		Macro_Log = FString::Printf(TEXT("[No Connection] [%s]"), *Macro_FunctionInfo);\
 	}\
-	\
+\
+\
 	const FString Macro_NullText = TEXT("");\
 	if (Macro_NullText != Format)\
 	{\
-		UE_LOG(LogStereoMixNetwork, Verbosity, TEXT(" \n\t%s\n\t%s"), *Macro_Log, *FString::Printf(Format, ##__VA_ARGS__));\
+		UE_LOG(LogStereoMixNetwork, Verbosity, TEXT("%s: %s"), *Macro_Log, *FString::Printf(Format, ##__VA_ARGS__));\
 	}\
 	else\
 	{\
-		UE_LOG(LogStereoMixNetwork, Verbosity, TEXT(" \n\t%s"), *Macro_Log);\
+		UE_LOG(LogStereoMixNetwork, Verbosity, TEXT("%s"), *Macro_Log);\
 	}\
 }
 #endif
