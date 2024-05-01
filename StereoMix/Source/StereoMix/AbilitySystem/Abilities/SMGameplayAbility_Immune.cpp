186dad1d5c499de196c898b1dc05f109e70b9110
TD 이동우 <mintyway0112@gmail.com>
2024-05-01 19:06:46 +0900

    면역 GA 생성

---
+++ StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGameplayAbility_Immune.cpp
@@ -0,0 +1,9 @@
+// Copyright Surround, Inc. All Rights Reserved.
+
+
+#include "SMGameplayAbility_Immune.h"
+
+void USMGameplayAbility_Immune::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
---
+++ StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGameplayAbility_Immune.h
@@ -0,0 +1,22 @@
+// Copyright Surround, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SMGameplayAbility.h"
+#include "SMGameplayAbility_Immune.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEREOMIX_API USMGameplayAbility_Immune : public USMGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+protected:
+	
+};
