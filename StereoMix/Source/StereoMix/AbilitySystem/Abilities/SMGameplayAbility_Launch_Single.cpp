cf47f64050e2fbe76b5eb78e1e26ea4f9e64b66b
TD 이동우 <mintyway0112@gmail.com>
2024-06-18 03:04:33 +0900

    투사체 발사 클래스 세분화 및 추가

---
+++ StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGameplayAbility_Launch_Single.cpp
@@ -0,0 +1,4 @@
+// Copyright Surround, Inc. All Rights Reserved.
+
+
+#include "SMGameplayAbility_Launch_Single.h"
---
+++ StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGameplayAbility_Launch_Single.h
@@ -0,0 +1,18 @@
+// Copyright Surround, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SMGameplayAbility_Launch.h"
+#include "SMGameplayAbility_Launch_Single.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEREOMIX_API USMGameplayAbility_Launch_Single : public USMGameplayAbility_Launch
+{
+	GENERATED_BODY()
+
+
+};
