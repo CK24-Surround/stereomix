2c9a231be88884253867a5ca3ef73ddeb74cfc4b
TD 이동우 <mintyway0112@gmail.com>
2024-06-19 14:48:32 +0900

    탄 퍼짐 구현

--- StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGameplayAbility_Launch_ElectricGuitar.h
+++ StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGameplayAbility_Launch_ElectricGuitar.h
@@ -22,6 +22,9 @@ class STEREOMIX_API USMGameplayAbility_Launch_ElectricGuitar : public USMGamepla
 {
 	GENERATED_BODY()
 
+public:
+	USMGameplayAbility_Launch_ElectricGuitar();
+
 protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 
@@ -29,6 +32,7 @@ protected:
 
 	void LaunchProjectileTimerCallback();
 
+	/** 투사체를 발사합니다. LaunchCount는 몇 번째 발사인지 나타냅니다. */
 	UFUNCTION(Server, Reliable)
 	void ServerRPCLaunchProjectile(const FVector_NetQuantize10& SourceLocation, const FVector_NetQuantizeNormal& Normal, int LaunchCount);
 
@@ -36,10 +40,10 @@ protected:
 	int32 ProjectileCount = 3;
 
 	UPROPERTY(EditAnywhere, Category = "Design|ElectricGuitar", DisplayName = "투사체가 발사될 넓이")
-	float ProjectileWidth = 50.0f;
+	float ProjectileWidth = 0.0f;
 
 	UPROPERTY(EditAnywhere, Category = "Design|ElectricGuitar", DisplayName = "투사체가 발사될 각도")
-	float ProjectileTotalAngle = 10.0f;
+	float ProjectileTotalAngle = 5.0f;
 
 	UPROPERTY(EditAnywhere, Category = "Design|ElectricGuitar", DisplayName = "전탄 발사 소요 시간")
 	float TotalLaunchTime = 0.175f;
