70bb0f2137c656f17fbc88f41f43114a1bb8a3b7
TD 이동우 <mintyway0112@gmail.com>
2024-06-23 19:05:58 +0900

    일렉기타, 피아노 캐릭터 서브클래스 생성

---
+++ StereoMix/Source/StereoMix/Characters/SMPlayerCharacter_Piano.cpp
@@ -0,0 +1,19 @@
+// Copyright Surround, Inc. All Rights Reserved.
+
+
+#include "SMPlayerCharacter_Piano.h"
+
+
+ASMPlayerCharacter_Piano::ASMPlayerCharacter_Piano() {}
+
+void ASMPlayerCharacter_Piano::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ASMPlayerCharacter_Piano::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
