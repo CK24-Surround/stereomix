2315f24a859c4f94b44100dae542e5380b8b374a
TD 이동우 <mintyway0112@gmail.com>
2024-06-23 20:36:57 +0900

    피아노 투사체 발사 구현

--- StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGameplayAbility_Launch_Piano.cpp
+++ StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGameplayAbility_Launch_Piano.cpp
@@ -3,7 +3,107 @@
 
 #include "SMGameplayAbility_Launch_Piano.h"
 
+#include "Characters/SMPlayerCharacter.h"
+#include "Games/SMGameMode.h"
+#include "Projectiles/SMProjectile.h"
+#include "Projectiles/SMProjectilePool.h"
+#include "Utilities/SMLog.h"
+
 void USMGameplayAbility_Launch_Piano::LaunchProjectile(const FVector& InStartLocation, const FVector& InNormal)
 {
-	
+	ASMPlayerCharacter* SourceCharacter = GetSMPlayerCharacterFromActorInfo();
+	if (!SourceCharacter)
+	{
+		NET_LOG(nullptr, Warning, TEXT("소스 캐릭터가 무효합니다."));
+		EndAbilityByCancel();
+		return;
+	}
+
+	USMAbilitySystemComponent* SourceASC = GetSMAbilitySystemComponentFromActorInfo();
+	if (!ensureAlways(SourceASC))
+	{
+		EndAbilityByCancel();
+		return;
+	}
+
+	LaunchData.SourceLocation = InStartLocation;
+	LaunchData.SpawnRotationOffset.Empty();
+	LaunchData.Count = 0;
+	LaunchData.Rate = TotalLaunchTime / static_cast<float>(ProjectileCount - 1);
+	LaunchData.bIsInverse = !LaunchData.bIsInverse;
+
+	const float StartYawRotation = -ProjectileTotalAngle / 2.0f;
+	const float SegmentYawRotation = ProjectileTotalAngle / (ProjectileCount - 1);
+	const FRotator SourceRotation = InNormal.Rotation();
+	for (int32 i = 0; i < ProjectileCount; ++i)
+	{
+		const float OffsetRotationYaw = i * SegmentYawRotation;
+		const FRotator NewRotation(0.0, StartYawRotation + OffsetRotationYaw, 0.0);
+		LaunchData.SpawnRotationOffset.Add(SourceRotation + NewRotation);
+	}
+
+	if (LaunchData.bIsInverse)
+	{
+		Algo::Reverse(LaunchData.SpawnRotationOffset);
+	}
+
+	// 첫발은 즉시 발사합니다.
+	LaunchProjectileCallback();
+}
+
+void USMGameplayAbility_Launch_Piano::LaunchProjectileCallback()
+{
+	ASMPlayerCharacter* SourceCharacter = GetSMPlayerCharacterFromActorInfo();
+	if (!SourceCharacter)
+	{
+		EndAbilityByCancel();
+		return;
+	}
+
+	USMAbilitySystemComponent* SourceASC = GetSMAbilitySystemComponentFromActorInfo();
+	if (!ensureAlways(SourceASC))
+	{
+		return;
+	}
+
+	ASMGameMode* CachedSMGameMode = GetWorld()->GetAuthGameMode<ASMGameMode>();
+	if (!ensureAlways(CachedSMGameMode))
+	{
+		EndAbilityByCancel();
+		return;
+	}
+
+	const ESMTeam SourceTeam = SourceCharacter->GetTeam();
+	if (SourceTeam == ESMTeam::None)
+	{
+		EndAbilityByCancel();
+		return;
+	}
+
+	USMProjectilePool* ProjectilePool = CachedSMGameMode->GetEletricGuitarProjectilePool(SourceTeam); // 임시로 피아노 투사체 대신 일렉기타 투사체 풀에 접근합니다. 
+	if (!ensureAlways(ProjectilePool))
+	{
+		EndAbilityByCancel();
+		return;
+	}
+
+	ASMProjectile* NewProjectile = ProjectilePool->GetProjectile(); // 투사체 풀에서 투사체를 가져옵니다.
+	if (!NewProjectile)
+	{
+		EndAbilityByCancel();
+		return;
+	}
+
+	const FVector LaunchLocation = bIsAttach ? SourceCharacter->GetActorLocation() : LaunchData.SourceLocation;
+	NewProjectile->Launch(SourceCharacter, LaunchLocation, LaunchData.SpawnRotationOffset[LaunchData.Count++].Vector(), ProjectileSpeed, MaxDistance, Damage); // 투사체를 발사합니다.
+
+	if (LaunchData.Count < ProjectileCount)
+	{
+		FTimerHandle TimerHandle;
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &ThisClass::LaunchProjectileCallback, LaunchData.Rate);
+	}
+	else
+	{
+		K2_EndAbility();
+	}
 }
--- StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGameplayAbility_Launch_Piano.h
+++ StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGameplayAbility_Launch_Piano.h
@@ -6,14 +6,39 @@
 #include "SMGameplayAbility_Launch_Single.h"
 #include "SMGameplayAbility_Launch_Piano.generated.h"
 
+struct FPianoLaunchData
+{
+	FVector SourceLocation;
+	TArray<FRotator> SpawnRotationOffset;
+	int32 Count = 0;
+	float Rate = 0.0f;
+	uint32 bIsInverse:1 = false;
+};
+
 /**
  * 
  */
-UCLASS()
+UCLASS(Abstract)
 class STEREOMIX_API USMGameplayAbility_Launch_Piano : public USMGameplayAbility_Launch_Single
 {
 	GENERATED_BODY()
 
 protected:
 	virtual void LaunchProjectile(const FVector& InStartLocation, const FVector& InNormal) override;
+
+	void LaunchProjectileCallback();
+
+	UPROPERTY(EditAnywhere, Category = "Design|Piano")
+	int32 ProjectileCount = 5;
+
+	UPROPERTY(EditAnywhere, Category = "Design|Piano")
+	float ProjectileTotalAngle = 45.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Design|Piano")
+	float TotalLaunchTime = 0.25f;
+
+	UPROPERTY(EditAnywhere, Category = "Design|Piano")
+	uint32 bIsAttach:1 = true;
+
+	FPianoLaunchData LaunchData;
 };
