b2818094c5844ea969d44c99969dd3f3258bc0ef
TD 이동우 <mintyway0112@gmail.com>
2024-06-27 00:29:29 +0900

    베이스 캐릭터 추가

---
+++ StereoMix/Source/StereoMix/Characters/SMPlayerCharacter_Bass.cpp
@@ -0,0 +1,21 @@
+// Copyright Surround, Inc. All Rights Reserved.
+
+
+#include "SMPlayerCharacter_Bass.h"
+
+
+ASMPlayerCharacter_Bass::ASMPlayerCharacter_Bass()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ASMPlayerCharacter_Bass::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASMPlayerCharacter_Bass::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ StereoMix/Source/StereoMix/Characters/SMPlayerCharacter_Bass.h
@@ -0,0 +1,22 @@
+// Copyright Surround, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SMPlayerCharacter.h"
+#include "SMPlayerCharacter_Bass.generated.h"
+
+UCLASS()
+class STEREOMIX_API ASMPlayerCharacter_Bass : public ASMPlayerCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASMPlayerCharacter_Bass();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
