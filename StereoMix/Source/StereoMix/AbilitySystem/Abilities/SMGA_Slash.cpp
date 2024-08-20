67d3745e1dc23c6d5a974abd6ed545ecb152a2a5
TD 이동우 <mintyway0112@gmail.com>
2024-08-20 19:42:21 +0900

    베기 쿨타임 적용

--- StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGA_Slash.cpp
+++ StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGA_Slash.cpp
@@ -3,7 +3,9 @@
 
 #include "SMGA_Slash.h"
 
+#include "AbilitySystem/SMTags.h"
 #include "Characters/SMPlayerCharacterBase.h"
+#include "Utilities/SMLog.h"
 
 void USMGA_Slash::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
 {
@@ -16,6 +18,23 @@ void USMGA_Slash::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const
 		return;
 	}
 
-	GEngine->AddOnScreenDebugMessage(INDEX_NONE, 1.0f, FColor::Yellow, TEXT("베기!"));
+	K2_CommitAbility();
+	NET_VLOG(SourceCharacter, -1, 3.0f, TEXT("베기"));
+
 	K2_EndAbility();
+}
+
+void USMGA_Slash::ApplyCooldown(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const
+{
+	const UGameplayEffect* CooldownGE = GetCooldownGameplayEffect();
+	if (CooldownGE)
+	{
+		const FGameplayEffectSpecHandle GESpecHandle = MakeOutgoingGameplayEffectSpec(CooldownGE->GetClass());
+		if (ensureAlways(GESpecHandle.IsValid()))
+		{
+			FGameplayEffectSpec* GESpec = GESpecHandle.Data.Get();
+			GESpec->SetSetByCallerMagnitude(SMTags::Data::Cooldown, Cooldown);
+			(void)ApplyGameplayEffectSpecToOwner(Handle, ActorInfo, ActivationInfo, GESpecHandle);
+		}
+	}
 }
--- StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGA_Slash.h
+++ StereoMix/Source/StereoMix/AbilitySystem/Abilities/SMGA_Slash.h
@@ -14,6 +14,11 @@ class STEREOMIX_API USMGA_Slash : public USMGameplayAbility
 {
 	GENERATED_BODY()
 
-public:
+protected:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	virtual void ApplyCooldown(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) const override;
+
+	UPROPERTY(EditAnywhere, Category = "Design")
+	float Cooldown = 1.0f;
 };
