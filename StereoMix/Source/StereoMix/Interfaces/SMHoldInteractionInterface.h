9a8e1b9f79c1c7dbdf46715961d8a1c662aec70b
TD 이동우 <mintyway0112@gmail.com>
2024-08-29 21:17:09 +0900

    HII(잡기 상호작용 인터페이스) 추가

---
+++ StereoMix/Source/StereoMix/Interfaces/SMHoldInteractionInterface.h
@@ -0,0 +1,25 @@
+// Copyright Surround, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "SMHoldInteractionInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class USMHoldInteractionInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class STEREOMIX_API ISMHoldInteractionInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
