806177dd4028abc5b32dfb23e47520c04945985c
TD 이동우 <mintyway0112@gmail.com>
2024-09-11 16:53:49 +0900

    일렉기타 클래스 추가

--- StereoMix/Source/StereoMix/Characters/Player/SMPlayerCharacterBase.cpp
+++ StereoMix/Source/StereoMix/Characters/Player/SMPlayerCharacterBase.cpp
@@ -127,7 +127,7 @@ void ASMPlayerCharacterBase::PostInitializeComponents()
 {
 	Super::PostInitializeComponents();
 
-	if (!ensureAlways(DataAsset))
+	if (!DataAsset)
 	{
 		return;
 	}
---
+++ StereoMix/Source/StereoMix/Data/Character/SMElectricGuitarCharacterDataAsset.h
@@ -0,0 +1,16 @@
+// Copyright Surround, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SMPlayerCharacterDataAsset.h"
+#include "SMElectricGuitarCharacterDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEREOMIX_API USMElectricGuitarCharacterDataAsset : public USMPlayerCharacterDataAsset
+{
+	GENERATED_BODY()
+};
