e852f3b5c49db2736335bc128b45d0cbc107380f
TD 이동우 <mintyway0112@gmail.com>
2024-09-21 03:48:11 +0900

    스타트 포인트 구현

---
+++ StereoMix/Source/StereoMix/Characters/StartPoint/SMPlayerStartBase.h
@@ -0,0 +1,13 @@
+// Copyright Studio Surround. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "SMPlayerStartBase.generated.h"
+
+UCLASS(Abstract)
+class STEREOMIX_API ASMPlayerStartBase : public APlayerStart
+{
+	GENERATED_BODY()
+};
