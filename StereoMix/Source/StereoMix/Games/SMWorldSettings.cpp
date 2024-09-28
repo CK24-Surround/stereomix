99fd201fc9f47b638d297e83ab26e59dc42eeead
플밍 곽필경 <pid011@outlook.com>
2024-09-29 01:49:10 +0900

    SMWorldSettings 추가

---
+++ StereoMix/Source/StereoMix/Games/SMWorldSettings.cpp
@@ -0,0 +1,13 @@
+// Copyright Studio Surround. All Rights Reserved.
+
+
+#include "SMWorldSettings.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(SMWorldSettings)
+
+void ASMWorldSettings::CheckForErrors()
+{
+	Super::CheckForErrors();
+
+	// 맵 에러 검출 로직 구현
+}
---
+++ StereoMix/Source/StereoMix/Games/SMWorldSettings.h
@@ -0,0 +1,27 @@
+// Copyright Studio Surround. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/WorldSettings.h"
+#include "SMWorldSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS(DisplayName = "StereoMix World Settings")
+class STEREOMIX_API ASMWorldSettings : public AWorldSettings
+{
+	GENERATED_BODY()
+
+public:
+#if WITH_EDITOR
+	virtual void CheckForErrors() override;
+#endif
+
+#if WITH_EDITORONLY_DATA
+	// 설정 시 에디터에서 PIE를 실행했을 때 Standalone 모드로 강제 전환됩니다.
+	UPROPERTY(EditDefaultsOnly, Category=PIE)
+	bool bForceStandaloneNetMode = false;
+#endif
+};
