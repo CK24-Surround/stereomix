ce39dfca5520dce690d87c36c7b1d91ea639b617
플밍 이승준 <leesj.dev@gmail.com>
2024-10-06 01:26:50 +0900

    Score Manager 추가

---
+++ StereoMix/Source/StereoMix/Components/Core/SMScoreManagerComponent.h
@@ -0,0 +1,23 @@
+﻿// Copyright Studio Surround. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SMScoreManagerComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class STEREOMIX_API USMScoreManagerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	USMScoreManagerComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
