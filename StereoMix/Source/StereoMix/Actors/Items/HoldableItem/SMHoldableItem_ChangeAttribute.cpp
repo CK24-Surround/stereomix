5d831000ebdd8eb1986ec823a0280febd15a3670
플밍 이승준 <leesj.dev@gmail.com>
2024-10-08 19:13:38 +0900

    Holdable Item 로직 수정

--- StereoMix/Source/StereoMix/Actors/Items/HoldableItem/SMHoldableItem_ChangeAttribute.cpp
+++ StereoMix/Source/StereoMix/Actors/Items/HoldableItem/SMHoldableItem_ChangeAttribute.cpp
@@ -3,10 +3,13 @@
 
 #include "SMHoldableItem_ChangeAttribute.h"
 
+#include "NiagaraComponent.h"
 #include "StereoMixLog.h"
+#include "AbilitySystem/SMAbilitySystemComponent.h"
 #include "Actors/Character/Player/SMPlayerCharacterBase.h"
 #include "Components/SphereComponent.h"
 #include "Components/Item/SMHIC_HealItem.h"
+#include "FunctionLibraries/SMAbilitySystemBlueprintLibrary.h"
 #include "Utilities/SMCollision.h"
 
 
@@ -22,16 +25,57 @@ void ASMHoldableItem_ChangeAttribute::ActivateItemByNoiseBreak(const UWorld* Wor
 {
 	Super::ActivateItemByNoiseBreak(World, TilesToBeCaptured, InActivator, TeamOption);
 
-	UE_LOG(LogStereoMix, Warning, TEXT("ASMHoldableItem_Heal::ActivateItemByNoiseBreak"));
+	USMAbilitySystemComponent* ActivatorASC = USMAbilitySystemBlueprintLibrary::GetSMAbilitySystemComponent(InActivator);
+	if (!ActivatorASC)
+	{
+		return;
+	}
+
+	bActivated = true;
 
 	ActivateItem(InActivator);
-	Destroy();
+
+	FGameplayEffectSpecHandle GESpec = ActivatorASC->MakeOutgoingSpec(SelfGE, 1.0f, ActivatorASC->MakeEffectContext());
+	if (GESpec.IsValid())
+	{
+		GESpec.Data->SetSetByCallerMagnitude(SelfDataTag, SelfMagnitude);
+		ActivatorASC->BP_ApplyGameplayEffectSpecToSelf(GESpec);
+	}
+
+	if (Duration <= 0.0f)
+	{
+		return;
+	}
+
+	CachedTiles = TilesToBeCaptured;
+	UE_LOG(LogStereoMix, Warning, TEXT("CachedTiles.Num(): %d"), CachedTiles.Num());
+
+	TWeakObjectPtr<ASMHoldableItem_ChangeAttribute> ThisWeakPtr(this);
+	World->GetTimerManager().SetTimer(TriggerCountTimerHandle, [World, ThisWeakPtr] {
+		if (ThisWeakPtr.IsValid())
+		{
+			ThisWeakPtr->CurrentTriggerCount += 1;
+			if (ThisWeakPtr->CurrentTriggerCount < ThisWeakPtr->TriggerCount)
+			{
+				ThisWeakPtr->TriggerCountTimerCallback();
+			}
+			else
+			{
+				if (World)
+				{
+					World->GetTimerManager().ClearTimer(ThisWeakPtr->TriggerCountTimerHandle);
+				}
+				ThisWeakPtr->Destroy();
+			}
+		}
+	}, Duration / TriggerCount, true);
 }
 
 void ASMHoldableItem_ChangeAttribute::OnHeldStateEntry()
 {
-	ColliderComponent->SetCollisionProfileName(SMCollisionProfileName::NoCollision);
 	MeshComponent->SetVisibility(false);
+	NiagaraComponent->SetVisibility(false);
+	ColliderComponent->SetCollisionProfileName(SMCollisionProfileName::NoCollision);
 	if (ASMPlayerCharacterBase* HoldingMePlayer = Cast<ASMPlayerCharacterBase>(HIC->GetActorHoldingMe()))
 	{
 		HoldingMePlayer->GetHoldInteractionComponent()->OnHeldStateExit.AddUObject(this, &ThisClass::OnHeldStateExit);
@@ -40,6 +84,19 @@ void ASMHoldableItem_ChangeAttribute::OnHeldStateEntry()
 
 void ASMHoldableItem_ChangeAttribute::OnHeldStateExit()
 {
-	ActivateItem(nullptr);
-	Destroy();
+	if (!bActivated)
+	{
+		Destroy();
+	}
+}
+
+void ASMHoldableItem_ChangeAttribute::BeginDestroy()
+{
+	UE_LOG(LogStereoMix, Warning, TEXT("Destroy called"));
+	Super::BeginDestroy();
+}
+
+void ASMHoldableItem_ChangeAttribute::TriggerCountTimerCallback()
+{
+	UE_LOG(LogStereoMix, Warning, TEXT("TriggerCountTimerCallback"));
 }
--- StereoMix/Source/StereoMix/Actors/Items/HoldableItem/SMHoldableItem_ChangeAttribute.h
+++ StereoMix/Source/StereoMix/Actors/Items/HoldableItem/SMHoldableItem_ChangeAttribute.h
@@ -3,8 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "SMHoldableItemBase.h"
 #include "SMHoldableItem_ChangeAttribute.generated.h"
+
+class UGameplayEffect;
 
 UCLASS()
 class STEREOMIX_API ASMHoldableItem_ChangeAttribute : public ASMHoldableItemBase
@@ -19,4 +22,41 @@ public:
 	virtual void OnHeldStateEntry() override;
 
 	virtual void OnHeldStateExit() override;
+
+	virtual void BeginDestroy() override;
+
+protected:
+	void TriggerCountTimerCallback();
+
+	UPROPERTY(EditAnywhere, Category = "Design")
+	TSubclassOf<UGameplayEffect> SelfGE;
+
+	UPROPERTY(EditAnywhere, Category = "Design")
+	FGameplayTag SelfDataTag;
+
+	UPROPERTY(EditAnywhere, Category = "Design")
+	float SelfMagnitude = 10.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Design")
+	TSubclassOf<UGameplayEffect> GE;
+
+	UPROPERTY(EditAnywhere, Category = "Design")
+	FGameplayTag DataTag;
+
+	UPROPERTY(EditAnywhere, Category = "Design")
+	float TotalMagnitude = 100.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Design", meta = (ClampMin = "0.0"))
+	float Duration = 5.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Design", meta = (ClampMin = "1"))
+	int32 TriggerCount = 1;
+
+	TArray<TObjectPtr<ASMTile>> CachedTiles = TArray<TObjectPtr<ASMTile>>();
+
+	FTimerHandle TriggerCountTimerHandle;
+
+	int32 CurrentTriggerCount = 0;
+
+	int8 bActivated:1 = false;
 };
