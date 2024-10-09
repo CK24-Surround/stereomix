3b763cb7cf05c29ce1a545b583bafe8f50067320
플밍 이승준 <leesj.dev@gmail.com>
2024-10-09 21:32:28 +0900

    AI Base 추가

---
+++ StereoMix/Source/StereoMix/Actors/Character/AI/SMAICharacterBase.cpp
@@ -0,0 +1,55 @@
+﻿// Copyright Studio Surround. All Rights Reserved.
+
+
+#include "SMAICharacterBase.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Utilities/SMCollision.h"
+
+
+ASMAICharacterBase::ASMAICharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	USkeletalMeshComponent* CachedMeshComponent = GetMesh();
+	CachedMeshComponent->SetCollisionProfileName(SMCollisionProfileName::NoCollision);
+	CachedMeshComponent->SetRelativeRotation(FRotator(0.0, -90.0, 0.0));
+	CachedMeshComponent->bRenderCustomDepth = true;
+	CachedMeshComponent->bReceivesDecals = false;
+
+	UCharacterMovementComponent* CachedMovementComponent = GetCharacterMovement();
+	CachedMovementComponent->MaxAcceleration = 9999.0f;
+	CachedMovementComponent->MaxStepHeight = 10.0f;
+	CachedMovementComponent->SetWalkableFloorAngle(5.0f);
+	CachedMovementComponent->bCanWalkOffLedges = false;
+
+	HitBox = CreateDefaultSubobject<UCapsuleComponent>(TEXT("HitBox"));
+	HitBox->SetupAttachment(RootComponent);
+	HitBox->SetCollisionProfileName(SMCollisionProfileName::Player);
+	HitBox->InitCapsuleSize(125.0f, 125.0f);
+}
+
+void ASMAICharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+AActor* ASMAICharacterBase::GetLastAttacker() const
+{
+	return nullptr;
+}
+
+void ASMAICharacterBase::SetLastAttacker(AActor* NewAttacker)
+{
+}
+
+void ASMAICharacterBase::ReceiveDamage(AActor* NewAttacker, float InDamageAmount)
+{
+}
+
+void ASMAICharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ StereoMix/Source/StereoMix/Actors/Character/AI/SMAICharacterBase.h
@@ -0,0 +1,36 @@
+﻿// Copyright Studio Surround. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Character/SMCharacterBase.h"
+#include "Interfaces/SMDamageInterface.h"
+#include "SMAICharacterBase.generated.h"
+
+UCLASS()
+class STEREOMIX_API ASMAICharacterBase : public ASMCharacterBase, public ISMDamageInterface
+{
+	GENERATED_BODY()
+
+public:
+	ASMAICharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, Category = "HitBox")
+	TObjectPtr<UCapsuleComponent> HitBox;
+
+public:
+	virtual AActor* GetLastAttacker() const override;
+	
+	virtual void SetLastAttacker(AActor* NewAttacker) override;
+	
+	virtual void ReceiveDamage(AActor* NewAttacker, float InDamageAmount) override;
+	
+	virtual bool CanIgnoreAttack() const override { return false; }
+	
+	virtual bool IsObstacle() override { return false; }
+
+	virtual void Tick(float DeltaTime) override;
+};
