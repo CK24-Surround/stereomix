5672b8a603d8a890a82986734277daafccfe6676
TD 이동우 <mintyway0112@gmail.com>
2024-10-13 18:43:32 +0900

    팀 함수 주석 추가

--- StereoMix/Source/StereoMix/FunctionLibraries/SMTeamBlueprintLibrary.h
+++ StereoMix/Source/StereoMix/FunctionLibraries/SMTeamBlueprintLibrary.h
@@ -26,11 +26,11 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Team")
 	static bool IsSameTeam(const AActor* lhs, const AActor* rhs);
 
-	/** 액터의 로컬 팀 정보를 반환합니다. 대상이 유효하지 않은 경우에는 다른 팀으로 반환합니다. */
+	/** 액터의 로컬 팀 정보를 반환합니다. 대상이 유효하지 않은 경우에는 다른 팀으로 반환합니다. 클라이언트에서 실행되어야합니다. */
 	UFUNCTION(BlueprintCallable, Category = "Team")
 	static ESMLocalTeam GetLocalTeam(AActor* TargetActor);
 
-	/** 로컬에서 플레이하고 있는 팀과 시전자의 팀이 같은지 확인합니다. */
+	/** 로컬에서 플레이하고 있는 팀과 시전자의 팀이 같은지 확인합니다. 클라이언트에서 실행되어야합니다. */
 	UFUNCTION(BlueprintCallable, Category = "Team")
 	static bool IsSameLocalTeam(AActor* TargetActor);
 };
