42970fb60b0508a9240c1ec8833cb5801b753248
플밍 이승준 <leesj.dev@gmail.com>
2024-10-15 01:37:43 +0900

    Score Board UI 리소스 추가

---
+++ StereoMix/Source/StereoMix/UI/Widget/ScoreBoard/SMPlaylist.h
@@ -0,0 +1,16 @@
+﻿// Copyright Studio Surround. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "SMPlaylist.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEREOMIX_API USMPlaylist : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+};
