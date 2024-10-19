e2c0900a52b3d48a2e5b1099e26fad2b5a89a507
플밍 이승준 <leesj.dev@gmail.com>
2024-10-20 00:21:29 +0900

    리소스 바인드

--- StereoMix/Source/StereoMix/UI/Widget/CharacterSelector/SMCharacterSelectorInformationWidget.cpp
+++ StereoMix/Source/StereoMix/UI/Widget/CharacterSelector/SMCharacterSelectorInformationWidget.cpp
@@ -2,3 +2,115 @@
 
 
 #include "SMCharacterSelectorInformationWidget.h"
+
+#include "CommonTextBlock.h"
+#include "Components/Image.h"
+
+void USMCharacterSelectorInformationWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	SkillImages.Add(SkillImage1);
+	SkillImages.Add(SkillImage2);
+	SkillImages.Add(SkillImage3);
+
+	SkillNames.Add(SkillName1);
+	SkillNames.Add(SkillName2);
+	SkillNames.Add(SkillName3);
+
+	SkillDescriptions.Add(SkillDescription1);
+	SkillDescriptions.Add(SkillDescription2);
+	SkillDescriptions.Add(SkillDescription3);
+
+	PlayerImages.Add(PlayerImage1);
+	PlayerImages.Add(PlayerImage2);
+	PlayerImages.Add(PlayerImage3);
+
+	PlayerNames.Add(PlayerName1);
+	PlayerNames.Add(PlayerName2);
+	PlayerNames.Add(PlayerName3);
+
+	PlayerCharacterTypes.Add(PlayerCharacterType1);
+	PlayerCharacterTypes.Add(PlayerCharacterType2);
+	PlayerCharacterTypes.Add(PlayerCharacterType3);
+}
+
+void USMCharacterSelectorInformationWidget::ResetInfo() const
+{
+	SelectedCharacterType->SetText(FText::FromString(""));
+
+	for (int32 i = 0; i < 3; ++i)
+	{
+		if (SkillImages.IsValidIndex(i))
+		{
+			SkillImages[i]->SetBrushFromTexture(nullptr);
+		}
+
+		if (SkillNames.IsValidIndex(i))
+		{
+			SkillNames[i]->SetText(FText::FromString(""));
+		}
+
+		if (SkillDescriptions.IsValidIndex(i))
+		{
+			SkillDescriptions[i]->SetText(FText::FromString(""));
+		}
+
+		if (PlayerImages.IsValidIndex(i))
+		{
+			PlayerImages[i]->SetBrushFromTexture(nullptr);
+		}
+
+		if (PlayerNames.IsValidIndex(i))
+		{
+			PlayerNames[i]->SetText(FText::FromString(""));
+		}
+
+		if (PlayerCharacterTypes.IsValidIndex(i))
+		{
+			PlayerCharacterTypes[i]->SetText(FText::FromString(""));
+		}
+	}
+}
+
+void USMCharacterSelectorInformationWidget::SetPlayerInfo(const TArray<FString>& InPlayerNames, const TArray<FString>& InPlayerCharacterTypes, const TArray<UTexture2D*>& InPlayerTextures)
+{
+	for (int32 i = 0; i < 3; ++i)
+	{
+		if (PlayerImages.IsValidIndex(i) && InPlayerTextures.IsValidIndex(i))
+		{
+			PlayerImages[i]->SetBrushFromTexture(InPlayerTextures[i]);
+		}
+
+		if (PlayerNames.IsValidIndex(i) && InPlayerNames.IsValidIndex(i))
+		{
+			PlayerNames[i]->SetText(FText::FromString(InPlayerNames[i]));
+		}
+
+		if (PlayerCharacterTypes.IsValidIndex(i) && InPlayerCharacterTypes.IsValidIndex(i))
+		{
+			PlayerCharacterTypes[i]->SetText(FText::FromString(InPlayerCharacterTypes[i]));
+		}
+	}
+}
+
+void USMCharacterSelectorInformationWidget::SetSkillInfo(const TArray<FString>& InSkillNames, const TArray<FString>& InSkillDescriptions, const TArray<UTexture2D*>& InSkillTextures)
+{
+	for (int32 i = 0; i < 3; ++i)
+	{
+		if (SkillImages.IsValidIndex(i) && InSkillTextures.IsValidIndex(i))
+		{
+			SkillImages[i]->SetBrushFromTexture(InSkillTextures[i]);
+		}
+
+		if (SkillNames.IsValidIndex(i) && InSkillNames.IsValidIndex(i))
+		{
+			SkillNames[i]->SetText(FText::FromString(InSkillNames[i]));
+		}
+
+		if (SkillDescriptions.IsValidIndex(i) && InSkillDescriptions.IsValidIndex(i))
+		{
+			SkillDescriptions[i]->SetText(FText::FromString(InSkillDescriptions[i]));
+		}
+	}
+}
--- StereoMix/Source/StereoMix/UI/Widget/CharacterSelector/SMCharacterSelectorInformationWidget.h
+++ StereoMix/Source/StereoMix/UI/Widget/CharacterSelector/SMCharacterSelectorInformationWidget.h
@@ -6,6 +6,8 @@
 #include "CommonUserWidget.h"
 #include "SMCharacterSelectorInformationWidget.generated.h"
 
+class UImage;
+class UCommonTextBlock;
 /**
  * 
  */
@@ -13,4 +15,85 @@ UCLASS()
 class STEREOMIX_API USMCharacterSelectorInformationWidget : public UCommonUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	virtual void NativeConstruct() override;
+
+	void ResetInfo() const;
+	
+	UFUNCTION(BlueprintCallable)
+	void SetPlayerInfo(const TArray<FString>& InPlayerNames, const TArray<FString>& InPlayerCharacterTypes, const TArray<UTexture2D*>& InPlayerTextures);
+	
+	UFUNCTION(BlueprintCallable)
+	void SetSkillInfo(const TArray<FString>& InSkillNames, const TArray<FString>& InSkillDescriptions, const TArray<UTexture2D*>& InSkillTextures);
+	
+protected:
+	TArray<TObjectPtr<UImage>> SkillImages;
+
+	TArray<TObjectPtr<UCommonTextBlock>> SkillNames;
+
+	TArray<TObjectPtr<UCommonTextBlock>> SkillDescriptions;
+
+	TArray<TObjectPtr<UImage>> PlayerImages;
+
+	TArray<TObjectPtr<UCommonTextBlock>> PlayerNames;
+
+	TArray<TObjectPtr<UCommonTextBlock>> PlayerCharacterTypes;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> SelectedCharacterType;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> SkillImage1;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> SkillName1;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> SkillDescription1;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> SkillImage2;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> SkillName2;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> SkillDescription2;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> SkillImage3;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> SkillName3;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> SkillDescription3;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> PlayerImage1;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> PlayerName1;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> PlayerCharacterType1;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> PlayerImage2;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> PlayerName2;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> PlayerCharacterType2;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> PlayerImage3;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> PlayerName3;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UCommonTextBlock> PlayerCharacterType3;
 };
