4f621f9ee925eaf0bf485a023858cd4fd2078e9a
플밍 이승준 <leesj.dev@gmail.com>
2024-10-19 19:49:20 +0900

    기본 틀 작업

---
+++ StereoMix/Source/StereoMix/UI/Widget/CharacterSelector/SMCharacterSelectorInformationWidget.h
@@ -0,0 +1,16 @@
+﻿// Copyright Studio Surround. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "SMCharacterSelectorInformationWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEREOMIX_API USMCharacterSelectorInformationWidget : public UCommonUserWidget
+{
+	GENERATED_BODY()
+};
